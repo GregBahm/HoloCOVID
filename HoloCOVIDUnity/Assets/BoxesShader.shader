f33ada6303c43aa22989af5dbce8a6b4f8d1b531
derp derp <workingbahm@yahoo.com.sg>
2020-04-10 04:36:11 -0700

    Started off with global population.

---
+++ HoloCOVIDUnity/Assets/BoxesShader.shader
@@ -0,0 +1,69 @@
+﻿Shader "Unlit/BoxesShader"
+{
+	Properties
+	{
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct PopulationPoint
+			{
+				float xOffset;
+				float zOffset;
+				float populationValue;
+			};
+
+            struct v2f
+            {
+				float popVal : TEXCOORD0;
+				float3 objSpace : TEXCOORD1;
+                float4 vertex : SV_POSITION;
+            };
+
+			StructuredBuffer<PopulationPoint> _PopulationData;
+
+			float4x4 _MasterTransform;
+			float _MaxValue;
+
+			float3 GetObjPos(float3 cubePos, PopulationPoint datum)
+			{
+				float retX = (cubePos.x / 360 + datum.xOffset) - .5;
+				float retZ = (cubePos.z / 180 + (1 - datum.zOffset)) - .5;
+				float retY = (cubePos.y + .5) * datum.populationValue / _MaxValue;
+				return float3(retX, retY, retZ);
+			}
+
+			v2f vert(appdata_full v, uint inst : SV_InstanceID)
+			{
+				PopulationPoint datum = _PopulationData[inst];
+
+				float3 objPoint = GetObjPos(v.vertex, datum);
+				float3 worldPos = mul(_MasterTransform, objPoint);
+
+                v2f o;
+
+                o.vertex = mul(UNITY_MATRIX_VP, float4(worldPos, 1.0f));
+				o.popVal = datum.populationValue / _MaxValue;
+				o.objSpace = v.vertex;
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+				float yVal = i.objSpace.y + .5;
+				return pow(i.popVal * yVal, .1);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ HoloCOVIDUnity/Assets/MainScript.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MainScript : MonoBehaviour
+{
+    private const int DataX = 360;
+    private const int DataY = 180;
+
+    [SerializeField]
+    private MeshRenderer boundsSource;
+
+    [SerializeField]
+    private TextAsset populationDataSource;
+
+    [SerializeField]
+    private Material mat;
+
+    [SerializeField]
+    private Mesh mesh;
+
+    private MeshRenderer meshRenderer;
+    private ComputeBuffer populationDataBuffer;
+
+    private float maxPopValue;
+
+    void Start()
+    {
+        PopulationDataPoint[] populationData = GetPopulationData().ToArray();
+        maxPopValue = populationData.Max(item => item.Population);
+        populationDataBuffer = GetPopulationDataBuffer(populationData);
+    }
+
+    private void OnDestroy()
+    {
+        populationDataBuffer.Dispose();
+    }
+
+    private ComputeBuffer GetPopulationDataBuffer(PopulationDataPoint[] populationData)
+    {
+        int stride = sizeof(float) + sizeof(float) + sizeof(float);
+        ComputeBuffer ret = new ComputeBuffer(populationData.Length, stride);
+        ret.SetData(populationData);
+        return ret;
+    }
+
+    private void Update()
+    {
+        mat.SetBuffer("_PopulationData", populationDataBuffer);
+        mat.SetFloat("_MaxValue", maxPopValue);
+        mat.SetMatrix("_MasterTransform", transform.localToWorldMatrix);
+        Graphics.DrawMeshInstancedProcedural(mesh, 0, mat, boundsSource.bounds, 360 * 180);
+    }
+
+    private IEnumerable<PopulationDataPoint> GetPopulationData()
+    {
+        string[] lines = populationDataSource.text.Split('\n');
+        for (int y = 0; y < DataY; y++)
+        {
+            string[] cells = lines[y].Split(' ');
+            for (int x = 0; x < DataX; x++)
+            {
+                float populationVal = Convert.ToSingle(cells[x]);
+                
+                if(populationVal > 0)
+                {
+                    float retX = (float)x / DataX;
+                    float retY = (float)y / DataY;
+                    yield return new PopulationDataPoint(retX, retY, populationVal);
+                }
+            }
+        }
+    }
+
+    private struct PopulationDataPoint
+    {
+        public float X { get; }
+        public float Y { get; }
+        public float Population { get; }
+
+        public PopulationDataPoint(float x, float y, float population)
+        {
+            X = x;
+            Y = y;
+            Population = population;
+        }
+    }
+}
